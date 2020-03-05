b5ab469248a47543da696c6d1258fab627f11eb5
EliteDaMyth <elitemythyt@gmail.com>
2020-03-05 14:14:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-for(const o of["all","countries"])exports[o]=(async()=>{const t=await require("node-fetch")(`https://corona.lmao.ninja/${o}`);if(200!==t.statusCode)throw new Error("Fail",res.statusCode);return await t.json()});
+for(const o of["all","countries"])exports[o]=(async()=>{const t=await require("node-fetch")(`https://corona.lmao.ninja/${o}`);if(200!==t.statusCode)throw new Error("Fail",t.statusCode);return await t.json()});
