5888e3d71ea6e3407f7485fe8c4914fb560b82e2
Firegore <firegore2@gmail.com>
2020-03-24 08:16:22 +0100

    new version added

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
+var _ = require('lodash');
 /*
   Enjoy the new functions I have created! More coming soon if diced or elite makes useful updates
   - Apollo
 */
 module.exports = {
-  all: async function() {
-    // Wait why did you .toString() this function?
-    const a = await require('node-fetch')(`https://corona.lmao.ninja/all`);return await a.json();
-  },
-  countries: async function(country) {
-    // Wait why did you .toString() this function?
-    if (!country) {
-         const a = await require('node-fetch')('https://corona.lmao.ninja/countries'); return await a.json();
-    } else if (country) {
-       try { const a = await require('node-fetch')(`https://corona.lmao.ninja/countries/${country}`); return await a.json();
-       } catch (e) { return { error: 'Country not found' } }
+    /**
+     * @returns {fetch.Promise<Array.<Object>>|undefined}
+     */
+    all: async function () {
+        // Wait why did you .toString() this function?
+        const a = await require('node-fetch')(`https://corona.lmao.ninja/all`);
+        return await a.json();
+    },
+    /**
+     *
+     * @param {?string} country
+     * @returns {fetch.Promise<Array.<Object>>|undefined}
+     */
+    countries: async function (country) {
+        // Wait why did you .toString() this function?
+        if (!country) {
+            const a = await require('node-fetch')('https://corona.lmao.ninja/countries');
+            return await a.json();
+        } else if (country) {
+            try {
+                const a = await require('node-fetch')(`https://corona.lmao.ninja/countries/${country}`);
+                return await a.json();
+            } catch (e) {
+                return {error: 'Country not found'}
+            }
+        }
+    },
+    /**
+     *
+     * @param {?string} state
+     * @returns {fetch.Promise<Array.<Object>>|undefined}
+     */
+    states: async function (state) {
+        const a = await require('node-fetch')('https://corona.lmao.ninja/states');
+        let data = await a.json();
+        return state ? data.find(el => el.state === state) : data;
+    },
+    /**
+     *
+     * @param {?Object} filter f.e. {country : "China"}
+     * @returns {fetch.Promise<Array.<Object>>|undefined}
+     */
+    jhucsse: async function (filter) {
+        const a = await require('node-fetch')('https://corona.lmao.ninja/jhucsse');
+        let data = await a.json();
+        if (!filter) return data;
+
+        filter = _.pick(filter, _(data).map(_.keys).thru(_.spread(_.union)).value());
+        return _.filter(data, filter);
     }
-  },
-  states: async function(state) {
-    // Wait why did you .toString() this function?
-    if (!state) { 
-       const a = await require('node-fetch')('https://corona.lmao.ninja/states');
-       return await a.json();
-    }
-    // Coming Soon VVV
-    // else if (state) try { const a = await require('node-fetch')(`https://corona.lmao.ninja/states/${state}`); return await a.json(); } catch(e) { return { error: 'State not found' } }
-  }
 }
