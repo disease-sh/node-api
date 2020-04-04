5ddee9eca00666329c214817d6179301f7c4c02e
Anish-Shobith <anish.shobith19@gmail.com>
2020-04-05 03:32:09 +0530

    feat: new endpoints and fixes

--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,32 @@ export class NovelCovid {
 	}
 
 	/**
+	 * @description Gets yesterday's information from the api.
+	 * @returns {Promise<Array<Country>>}
+	 */
+	async yesterday(): Promise<Array<Country>> {
+
+		return fetch(`${this.baseURL}/yesterday`).then(json);
+	}
+
+	/**
+	 * @description Gets all the effected country names.
+	 * @returns {Promise<Array<String>>}
+	 */
+	async countryNames(): Promise<Array<string>> {
+
+		const countries = await this.countries() as Country[] | null;
+
+		return countries!.map(x => x.country);
+	}
+
+	/**
 	 * @description Fetches data of corona virus by country.
-	 * @param {String} [country=null] - Country details you want to fetch.
-	 * @param {String} sort - Sort by active, deaths , etc.
+	 * @param {?string} [country=null] - Country details you want to fetch.
+	 * @param {?string} sort - Sort by active, deaths , etc.
 	 * @returns {Promise<Array<Country>| Country |null>}
 	 */
-	async countries(country?: string | null | number): Promise<Country | null>;
+	async countries(country?: string | null | number): Promise<Country | Array<Country> | null>;
 	async countries(country: null, sort?: keyof CountrySort): Promise<Array<Country>>;
 	async countries(country?: string | null | number, sort?: keyof Country): Promise<Array<Country> | Country | null> {
 
@@ -47,11 +67,11 @@ export class NovelCovid {
 
 	/**
 	 * @description Fetches data of corona virus in United States.
-	 * @param {String} [state=null] - State name data you wanna fetch.
+	 * @param {?string} [state=null] - State name data you wanna fetch.
 	 * @returns {Promise<Array<State> | State | null>}
 	 */
 	async states(): Promise<Array<State>>;
-	async states(state: string| null): Promise<State | null>;
+	async states(state: string | null): Promise<State | null>;
 	async states(state?: string | null): Promise<Array<State> | State | null> {
 
 		if (state) {
@@ -67,21 +87,21 @@ export class NovelCovid {
 
 	/**
 	 * @description Get historical data from the start of 2020. (JHU CSSE GISand Data).
-	 * @param {String} [all=null] - Returns  all the cases and deaths.
-	 * @param {String} [country=null] -  Returns data of a specific country.
-	 * @param {String} [province=null] - Get a province within a country's time series.
-	 * @returns {Promise<Array<Historical> | HistoricalAll | HistoricalCountry | null>}
+	 * @param {?boolean} [all=null] - Returns  all the cases and deaths.
+	 * @param {?string} [country=null] -  Returns data of a specific country.
+	 * @param {?string} [province=null] - Get a province within a country's time series.
+	 * @returns {Promise<Array<Historical> | HistoricalAll | HistoricalCountry | Array<HistoricalCountry> | null>}
 	 */
 	async historical(): Promise<Array<Historical>>;
-	async historical(all: boolean | null): Promise<HistoricalAll>;
-	async historical(all: null, country?: string | null, province?: string | null): Promise<HistoricalCountry | null>;
-	async historical(all?: boolean | null, country?: string | null, province?: string | null): Promise<Array<Historical> | HistoricalCountry | HistoricalAll | null> {
+	async historical(all?: boolean | null): Promise<HistoricalAll>;
+	async historical(all: null, country: string | null, province?: string | null): Promise<HistoricalCountry | Array<HistoricalCountry> | null>;
+	async historical(all?: boolean | null, country?: string | null, province?: string | null): Promise<Array<Historical> | HistoricalCountry | Array<HistoricalCountry> | HistoricalAll | null> {
 
-		if (country) {
+		if (country && !province && !all) {
 
 			return fetch(`${this.baseURL}/v2/historical/${country}`).then(json);
 
-		} else if (country && province) {
+		} else if (country && province && !all) {
 
 			return fetch(`${this.baseURL}/v2/historical/${country}/${province}`).then(json);
 
@@ -97,9 +117,24 @@ export class NovelCovid {
 
 	/**
 	 * @description Return data from the John Hopkins CSSE Data Repository (Provinces and such).
+	 * @param {?boolean} [countries=null] - If method should return counties
+	 * @param {?string} countryname - County name if counties is true.
 	 * @returns {Promise<Array<Jhucsse>>}
 	 */
-	async jhucsse(): Promise<Array<Jhucsse>> {
+	async jhucsse(): Promise<Array<Jhucsse>>;
+	async jhucsse(counties?: boolean | null): Promise<Array<JhucsseCounties>>;
+	async jhucsse(counties: boolean, countyname: string): Promise<JhucsseCountry>;
+	async jhucsse(counties?: boolean, countyname?: string): Promise<Array<Jhucsse> | Array<JhucsseCounties> | JhucsseCountry> {
+
+		if (counties) {
+
+			return fetch(`${this.baseURL}/v2/jhucsse/counties`).then(json);
+
+		} else if (counties && countyname) {
+
+			return fetch(`${this.baseURL}/v2/jhucsse/counties/${countyname}`).then(json);
+
+		}
 
 		return fetch(`${this.baseURL}/v2/jhucsse`).then(json);
 
@@ -126,13 +161,14 @@ export interface Country {
 		iso2: string;
 	};
 	cases: number;
-	todaysCases: number;
+	todayCases: number;
 	deaths: number;
 	recovered: number;
 	active: number;
 	critical: number;
 	casesPerOneMillion: number;
 	deathsPerOneMillion: number;
+	updated: number;
 }
 
 export interface State {
@@ -162,8 +198,31 @@ export interface HistoricalCountry {
 }
 export interface Jhucsse {
 	country: string;
+	province: Array<string> | string | null;
+	updatedAt: Date;
+	stats: {
+		confirmed: number;
+		deaths: number;
+		recovered: number;
+	};
+	coordinates: {
+		latitude: string;
+		longitude: string;
+	};
+}
+
+export interface JhucsseCountry {
+	country: string;
+	province: string | null;
+	county: string;
+	updatedAt: Date;
+}
+
+export interface JhucsseCounties {
+	country: string;
 	province: string | null;
 	updatedAt: Date;
+	county: string;
 	stats: {
 		confirmed: number;
 		deaths: number;
@@ -177,7 +236,7 @@ export interface Jhucsse {
 
 export interface CountrySort {
 	cases: number;
-	todaysCases: number;
+	todayCases: number;
 	deaths: number;
 	recovered: number;
 	active: number;
@@ -191,4 +250,3 @@ export interface HistoricalAll {
 	deaths: object;
 	recovered: object;
 }
-
